90030cf089d1d77bd8622431fc80e07d850721d7
Gerardo Bort <gerardobort@Gerardos-MacBook-Pro.local>
2015-12-25 23:31:01 -0300

    moved project, making compatible with OF0.9

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "ofxGui.h"
 #include "testApp.h"
-#include "ofAppGlutWindow.h"
 
 //--------------------------------------------------------------
 int main(){
-    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-    ofSetupOpenGL(1400, 940, OF_WINDOW);
+    ofGLWindowSettings settings;
+    settings.setGLVersion(3,2);
+    ofCreateWindow(settings);
     ofRunApp(new testApp());
 }
