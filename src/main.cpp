c249ea37090839b501d8697af4d178ae927744e3
Gerardo Bort <gerardobort@gmail.com>
2016-09-18 16:27:25 +0300

    raspberry: making the window display, shaders still not working

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@ int main(){
     //settings.setGLVersion(2, 1);
     ofGLESWindowSettings settings;
     settings.setGLESVersion(2);
-    settings.windowMode = OF_FULLSCREEN;
+    settings.windowMode = OF_WINDOW;
+    settings.width = 1200;
+    settings.height = 960;
     ofCreateWindow(settings);
     ofRunApp(new ofApp());
 }
